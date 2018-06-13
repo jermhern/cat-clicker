a5e13ea2256b243ee6a81b5dd516321ebdd4dadd
jeremy hernandez <jermhern@gmail.com>
2018-06-13 12:46:53 -0700

    added sounds

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // keep track of clicks
 let catClick = 0;
 
+// var for audio 
+const meow = new Audio('meow.mp3');
+
 // add one click to screen when player clicks on kitty
 document.querySelector('.cat').addEventListener('click', function() {
 	catClick++;
 	document.querySelector('.level-text').innerHTML = catClick;
+
+	if(catClick % 10 === 0) {
+		meow.play();
+	}
 });
