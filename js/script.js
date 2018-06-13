c2abc8ba2bf9738adb98f90bbc378997cfa2229f
jeremy hernandez <jermhern@gmail.com>
2018-06-13 12:55:03 -0700

    added design on click counter

--- js/script.js
+++ js/script.js
@@ -6,9 +6,14 @@ const meow = new Audio('meow.mp3');
 
 // add one click to screen when player clicks on kitty
 document.querySelector('.cat').addEventListener('click', function() {
+	
+	// add one to the cat clicks
 	catClick++;
+
+	// change the HTML on page to the cat clicks
 	document.querySelector('.level-text').innerHTML = catClick;
 
+	// play meow sound every 10 clicks 
 	if(catClick % 10 === 0) {
 		meow.play();
 	}
