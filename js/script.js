88708292a8e76f6af64c9173789ac41f74c4beb6
jeremy hernandez <jermhern@gmail.com>
2018-06-13 20:15:19 -0700

    added second cat with title

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ let catClick = 0;
 
 // var for audio 
 const meow = new Audio('meow.mp3');
+const humanMeow = new Audio('human.mp3');
+
+// add names to screen
+document.querySelector('.tibbyName').innerHTML = 'Tibby';
+document.querySelector('.mittsName').innerHTML = 'Mitts';
+
+
 
 // add one click to screen when player clicks on kitty
 document.querySelector('.cat').addEventListener('click', function() {
@@ -16,5 +23,16 @@ document.querySelector('.cat').addEventListener('click', function() {
 	// play meow sound every 10 clicks 
 	if(catClick % 10 === 0) {
 		meow.play();
+	}
+});
+
+document.querySelector('.catDos').addEventListener('click', function() {
+	
+	catClick++;
+
+	document.querySelector('.level-text').innerHTML = catClick;
+
+	if(catClick % 10 === 0) {
+		humanMeow.play();
 	}
 });
